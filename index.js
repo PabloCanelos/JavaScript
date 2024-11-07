c3a1842e973a0c46e24c59ee89444f74207b3e36
Pablo Canelos <pabloccanelos.contacto@gmail.com>
2024-11-07 12:30:40 -0300

    js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const aumenta = document.querySelector('#aumenta')
-const resta = document.querySelector('#resta')
+
+
+
+
+
+
+const suma = document.querySelector('#suma');
+const resta = document.querySelector('#resta');
+let cantidad = document.querySelector('.cantidad');
+let total = document.querySelector('.valor-total');
+
+let valorCantidad = 0;
+cantidad.innerHTML = valorCantidad;
+
+const precio = 400000;
+let precioUnitario = document.querySelector('.precio-inicial');
+precioUnitario.innerHTML = precio;
+
+suma.onclick = function() {
+    cantidad.innerHTML = `${++valorCantidad}`;
+    total.innerHTML = `${valorCantidad * precio}`;
+}
+
+resta.onclick = function() {
+    if (valorCantidad > 0) {
+        valorCantidad--;
+        cantidad.innerHTML = valorCantidad;
+        total.innerHTML = valorCantidad * precio;
+    }
+};
 
 
 
@@ -8,7 +36,3 @@ const resta = document.querySelector('#resta')
 
 
 
-precio = 400000
-
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
