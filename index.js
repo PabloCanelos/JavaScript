b5a0bc2bff18a7764952d1e3e460ee2acbc6a00a
Pablo Canelos <pabloccanelos.contacto@gmail.com>
2024-11-07 09:40:43 -0300

    agrego codigo inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+const aumenta = document.querySelector('#aumenta')
+const resta = document.querySelector('#resta')
+
+
+
+
+
+
+
+
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
